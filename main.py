13d3cfc9c19693e7fe7fdc013b5c6c78824b753a
Tumbo Lobi John <johnlobiit@gmail.com>
2022-08-29 08:58:22 +0300

    other methods of calculating sum of natural numbers

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ def fn(n):
 
 res = fn(5)
 print(res)
+
+
+def fn2(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum = sum + i
+    return sum
+
+
+res2 = fn2(5)
+print(res2)
+
+
+def fn3(n):
+    sum = 0
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            sum = sum + 1
+    return sum
+
+
+res3 = fn3(5)
+print(res3)
