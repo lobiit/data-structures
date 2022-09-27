66a0e117f4effd06061c432abde63dd23c60ccb2
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-27 08:39:41 +0300

    If we have one pointer moving twice as fast as the other, then by the time it reaches the end, the slow pointer will be halfway through since it is moving at half the speed.

--- linked lists/midddle of odd numbers.py
+++ linked lists/midddle of odd numbers.py
@@ -1,9 +1,19 @@
-def get_head(head):
+def get_middle(head):
     length = 0
     dummy = head
     while dummy:
         length += 1
         dummy = dummy.next
-    for i in range(length//2):
+    for i in range(length // 2):
         head = head.next
     return head.val
+
+
+# Fast and slow pointers
+def get_middle(head):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow.val
